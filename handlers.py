38e7df57822b19a815ccbfb469e4390f94e06e1a
Scott Thomas <sthomas@idis.com>
2020-10-06 18:46:02 -0600

    Added sample and genesis configmap

--- handlers.py
+++ handlers.py
@@ -1,8 +1,13 @@
 '''Handlers for the keth operator
 '''
 # pylint: disable=missing-function-docstring
+import os
 import datetime
+
 import kopf
+import yaml
+import kubernetes
+from kubernetes.client.rest import ApiException
 
 
 GROUP = 'kube-blockchain.io'
@@ -22,5 +27,34 @@ def get_current_timestamp(**_):
 @kopf.on.resume(GROUP, VERSION, PLURAL)
 @kopf.on.create(GROUP, VERSION, PLURAL)
 @kopf.on.update(GROUP, VERSION, PLURAL)
-def ensure_deployment(logger, **_):
+def ensure_deployment(name, namespace, logger, **_):
     logger.info('ensure_deployment')
+
+    config_map_genesis_tpl = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)),
+        "templates/config-map-genesis.yaml",
+    )
+    config_map_genesis_name=f'{name}-genesis'
+    with open(config_map_genesis_tpl, 'r') as template:
+        config_map_genesis = yaml.safe_load(template.read().format(
+            name=config_map_genesis_name,
+            namespace=namespace,
+        ))
+    kopf.adopt(config_map_genesis)
+    kubernetes.config.load_incluster_config()
+    apps = kubernetes.client.AppsV1Api()
+    core = kubernetes.client.CoreV1Api()
+    try:
+        core.create_namespaced_config_map(
+            namespace,
+            config_map_genesis,
+        )
+    except ApiException as error:
+        if error.status != 409:
+            raise
+
+        core.patch_namespaced_config_map(
+            config_map_genesis_name,
+            namespace,
+            config_map_genesis,
+        )
