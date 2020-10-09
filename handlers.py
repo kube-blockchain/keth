b0349113dd4b3a0b5dd153a15458fba7182afc91
Scott Thomas <sthomas@idis.com>
2020-10-08 23:44:44 -0600

    refactor configmap handler

--- handlers.py
+++ handlers.py
@@ -24,7 +24,7 @@ def _template_file(file):
 def _template_load(file, **kwargs):
     template_file = _template_file(file)
     with open(template_file, 'r') as template:
-        return yaml.safe_load(template.read().format(kwargs))
+        return yaml.safe_load(template.read().format(**kwargs))
 
 
 def _timestamp():
